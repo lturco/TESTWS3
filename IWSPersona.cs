196f6a4eafb07e2b311554381859d5c98b4129c9
IronSharq <lukitinchox@gmail.com>
2017-03-06 00:00:56 -0300

    very first commit

---
+++ IWSPersona.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.Serialization;
+using System.ServiceModel;
+using System.ServiceModel.Web;
+using System.Text;
+
+namespace WSTEST3
+{
+    // NOTA: puede usar el comando "Rename" del menú "Refactorizar" para cambiar el nombre de interfaz "IService1" en el código y en el archivo de configuración a la vez.
+    [ServiceContract]
+    public interface IWSPersona
+    {
+
+        [OperationContract]
+        Persona ObtenerInformacion(string identificacion);
+
+       
+    }
+
+
+    // Utilice un contrato de datos, como se ilustra en el ejemplo siguiente, para agregar tipos compuestos a las operaciones de servicio.
+    [DataContract]
+    public class Persona: BaseRespuesta
+    {
+        [DataMember]
+        public string nombre { get; set; }
+        [DataMember]
+        public int edad { get; set; }
+
+        public string secreto { get; set; }
+    }
+
+    [DataContract]
+    public class BaseRespuesta
+    {
+        public string error { get; set; }
+        public string mensaje { get; set; }
+    }
+
+    [DataContract]
+    public class CompositeType
+    {
+        bool boolValue = true;
+        string stringValue = "Hello ";
+
+        [DataMember]
+        public bool BoolValue
+        {
+            get { return boolValue; }
+            set { boolValue = value; }
+        }
+
+        [DataMember]
+        public string StringValue
+        {
+            get { return stringValue; }
+            set { stringValue = value; }
+        }
+    }
+}
---
+++ WSPersona.svc.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.Serialization;
+using System.ServiceModel;
+using System.ServiceModel.Web;
+using System.Text;
+using System.Data;
+using System.Data.SqlClient;
+using System.Data.Entity;
+
+namespace WSTEST3
+{
+    // NOTA: puede usar el comando "Rename" del menú "Refactorizar" para cambiar el nombre de clase "Service1" en el código, en svc y en el archivo de configuración.
+    // NOTE: para iniciar el Cliente de prueba WCF para probar este servicio, seleccione Service1.svc o Service1.svc.cs en el Explorador de soluciones e inicie la depuración.
+    public class WSPersona : IWSPersona
+    {
+        public Persona ObtenerInformacion(string identificacion)
+        {
+
+
+            try
+            {
+                string condicion = "-5";
+                SqlConnection conn = new SqlConnection();
+                conn.ConnectionString = "Server=2c493008-8b16-4b7c-a93b-a727002340a2.sqlserver.sequelizer.com;Database=db2c4930088b164b7ca93ba727002340a2;User ID=wgvbrufuitipenfo;Password=vy8Bcv33jQvdsPzaNucGWBSK5orR3x3gzeVS2feDu73aAjDNvt4WCLS3PvEVej8U;";
+                SqlDataAdapter da = new SqlDataAdapter("select * from personas where edad > " + condicion, conn);
+                SqlDataAdapter di = new SqlDataAdapter("insert into personas values('guillermo',52)", conn);
+
+                DataSet ds = new DataSet();
+                da.Fill(ds, "personas");
+
+
+                foreach (DataRow dato in ds.Tables["personas"].Rows)
+                {
+                    Persona tipo = new Persona() { nombre = dato["nombre"].ToString(), edad = int.Parse(dato["edad"].ToString()) };
+
+
+                }
+                 
+            }
+            catch (Exception x)
+            {
+                string hola = x.InnerException.ToString();
+
+            }
+
+            if (identificacion == "0")
+            {
+                return new Persona() { nombre = "Roberto", edad = 45 };
+            }
+            else
+            {
+                return new Persona() { error = "golasfnlajsfhsoifs" };
+            }
+        }
+
+        
+    }
+}
